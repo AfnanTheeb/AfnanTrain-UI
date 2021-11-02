478876a594f077e8a0b9c3b94f910a334a66359b
AfnanTheb <Afnan.Alotaibi.med.s@tuwaiq.edu.sa>
2021-11-02 23:01:32 +0300

    Create Afnan Service Model create client model create ticket model

--- AfnanTrainBook/Model/Client.swift
+++ AfnanTrainBook/Model/Client.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+//create struct for Client consist information the clients ..
+struct Client {
+    
+    var firstName : String               // declertion client_Info .
+    var lastName : String                //--------------------------
+    var birthYear : Int
+    let id : Int
+    var age : Int {
+        2021 - birthYear
+    }
+    //--------------------------------------------------
+    // to stor all client reservations insaied array .
+    var tickets: [Ticket] = []
+    //--------------------------------------------------
+    init (firstName : String , lastName : String , birthYear : Int , id : Int) {
+        self.birthYear = birthYear
+        self.lastName = lastName
+        self.firstName = firstName
+        self.id = id
+        
+    } // end function init
+    //---------------------------------------------------
+    
+    
+    mutating func deletTicket ( numTicket : String ){
+        
+        
+        for (index , ticket) in self.tickets.enumerated() {
+            if( ticket.ticketID == numTicket ){
+                print("ticket id :\(ticket.ticketID) is deleting ..")
+                self.tickets.remove(at: index)
+            }
+        }
+    }
+    
+}// end struct client
--- AfnanTrainBook/Model/Ticket.swift
+++ AfnanTrainBook/Model/Ticket.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+// First :
+// Declaration the Enumerations - Stations && class Booking
+enum Station : String {
+    case Makkah
+    case Jeddah         // only 4 stations of the train
+    case KAEC
+    case Madinah
+}
+//______________________________________________
+// Determining the booking class / business - economy
+enum BookingType : Int {
+    case Economey
+    case Business
+}
+//______________________________________________
+// create struct for ticket consist information the ticket and calculate cost for it
+//
+
+struct Ticket {
+    var startStation: Station
+    var endStation: Station
+    var cost: Float = 0
+    var bookingType: BookingType
+    static var lastId = 0
+    
+    static func getNewID() -> String {
+        lastId += 1
+        return (String(lastId))
+    }
+    var ticketID : String = "AFNA-000\(getNewID())"
+    
+}
