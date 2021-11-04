ff8d6c412d2caed0b46546898f04974689e306f3
AfnanTheb <Afnan.Alotaibi.med.s@tuwaiq.edu.sa>
2021-11-04 04:48:36 +0300

    fixed bug in toPicker and fromPicker select default UIPickerView issue Ticket inject reservation and ticket to RegisterViewController

--- AfnanTrainBook/Controller/BookingViewController.swift
+++ AfnanTrainBook/Controller/BookingViewController.swift
@@ -8,18 +8,35 @@
 import UIKit
 
 class BookingViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    var reservation : Reservation!
     
     @IBOutlet weak var fromPicker: UIPickerView!
     @IBOutlet weak var toPicker: UIPickerView!
-    
- 
     @IBOutlet weak var setmentClass: UISegmentedControl!
     
     var fromStations = ["Makkah", "Jeddah", "KAEC" , "Madinah"]
     var toStations = ["Makkah", "Jeddah", "KAEC" , "Madinah"]
-    var fromSelected , toSelected : String!
+    var fromSelected , toSelected : Station!
     var typeSelected : Int!
+    var ticket : Ticket!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fromPicker.delegate = self
+        fromPicker.dataSource = self
+        
+        toPicker.delegate = self
+        toPicker.dataSource = self
+        
+        typeSelected = 0
+        toPicker.isHidden = true
+        fromSelected = .Makkah
+        toSelected = .Jeddah
+        fromPicker.selectRow(0, inComponent: 0, animated: true)
+        toPicker.selectRow(1, inComponent: 0, animated: true)
+        ticket = issueTicketPrice(from: .Makkah, to: .Jeddah, ticketType: 0 )
+        // Do any additional setup after loading the view.
+    }
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -44,42 +61,40 @@ class BookingViewController: UIViewController, UIPickerViewDataSource, UIPickerV
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         if( pickerView == fromPicker){
             toStations = fromStations.filter({$0 != fromStations[row]})
-            fromSelected = fromStations[row]
+            fromSelected = Station(rawValue: fromStations[row])
+            ticket.startStation = fromSelected
+            updateTicketPrice()
             toPicker.isHidden = false
+            
         } else {
-            toSelected = toStations[row]
+            toSelected = Station(rawValue: toStations[row])
 
         }
+//        updatePriceTicket()
         fromPicker.reloadAllComponents()
         toPicker.reloadAllComponents()
 
     }
     
 
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        fromPicker.delegate = self
-        fromPicker.dataSource = self
-        
-        toPicker.delegate = self
-        toPicker.dataSource = self
-        
-        typeSelected = 0
-        toPicker.isHidden = true
-        // Do any additional setup after loading the view.
-    }
     
     @IBAction func onChange(_ sender: Any) {
         print (setmentClass.selectedSegmentIndex)
         typeSelected = setmentClass.selectedSegmentIndex
+//        updatePriceTicket()
+
     }
+    func updateTicketPrice() {
+        
+
+    }
+
     
-    func calculateTicketPrice(from: String, to: String, ticketType: Int = 0 ) -> Ticket {
-        let enumFromSelected = Station(rawValue: from)
-        let enumToSelected = Station(rawValue: to)
+    func issueTicketPrice(from: Station = .Makkah, to: Station = .Jeddah, ticketType: Int = 0 ) -> Ticket {
+        let enumFromSelected : Station = from
+        let enumToSelected : Station =  to
         let enumTicketType = BookingType(rawValue: ticketType)
-        return Ticket(startStation: enumFromSelected!, endStation: enumToSelected!, bookingType: enumTicketType!)
+        return Ticket(startStation: enumFromSelected, endStation: enumToSelected, cost: 40, bookingType: enumTicketType!)
     }
     
     // MARK: - Navigation
@@ -90,8 +105,9 @@ class BookingViewController: UIViewController, UIPickerViewDataSource, UIPickerV
 //        segue.destination
         if (segue.identifier == "createTicketSegue"){
             let ticketVC = segue.destination as! RegisterViewController
-            
-            ticketVC.ticket = calculateTicketPrice(from: fromSelected, to: toSelected, ticketType: typeSelected)
+            ticketVC.reservation = reservation
+
+            ticketVC.ticket = ticket
         }
         
         // Get the new view controller using segue.destination.
